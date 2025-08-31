e87bb7c066d5a5f25254d987fc7078a7a78baa1f
ClaudioK5 <klaudio.asaro5@gmail.com>
2025-08-31 12:43:31 +0200

    Update main.py

--- AWS s3 Archiver Pipeline + AI/main.py
+++ AWS s3 Archiver Pipeline + AI/main.py
@@ -1,10 +1,26 @@
-from aws_archive_tools import normalize_bucket
+from aws_archive_tools import s3_files_archiver
 from AI_report_maker import AI_report_maker
+from datetime import datetime, timedelta
+from aws_filetextretriever_tools import get_files_for_current_month
+import time
 
-bucket_name = "bucketname"
+bucket_name = "pythonaswbucket"
 
-print(f"Running normalization on bucket: {'bucketname'}")
 
-normalize_bucket(bucket_name)
+while True:
 
-print("Normalization successfully completed.")
+    today = datetime.today()
+
+
+    s3_files_archiver(bucket_name)
+
+
+    if (today + timedelta(days=1)).day == 1:
+
+        files = get_files_for_current_month(bucket_name)
+
+        AI_report_maker(files, bucket_name)
+
+    time.sleep(86400)
+
+
