aed41d44924a743176554daf54fa760456826466
ClaudioK5 <klaudio.asaro5@gmail.com>
2025-09-02 21:07:37 +0200

    Update email_sender.py

--- email_sender.py
+++ email_sender.py
@@ -1,15 +1,19 @@
 import sys
 import smtplib
 from email.message import EmailMessage
+import os
 
+bot_email = os.getenv("BOT_EMAIL")
+bot_email_password = os.getenv("BOT_EMAIL_PASSWORD")
 
 def send_email(to_email, subject, body):
     msg = EmailMessage()
     msg['Subject'] = subject
-    msg['From'] = 'insert here your bot email'
+    msg['From'] = bot_email
     msg['To'] = to_email
     msg.set_content(body)
 
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-        smtp.login('insert here your bot email','insert here your bot password')
+        smtp.login(bot_email,bot_email_password)
         smtp.send_message(msg)
+
