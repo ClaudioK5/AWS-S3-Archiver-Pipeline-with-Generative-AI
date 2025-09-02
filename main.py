b35d16762bff76ef587a01b911e370845eb004e8
ClaudioK5 <klaudio.asaro5@gmail.com>
2025-09-02 21:06:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from AI_report_maker import AI_report_maker
 from datetime import datetime, timedelta
 from aws_filetextretriever_tools import get_files_for_current_month
 import time
+import os
 
-bucket_name = "insert here your bucket name"
+bucket_name = os.getenv("BUCKET_NAME")
 
 #the archiviation operation will take place once everyday, meanwhile the
 #generative AI report will be made on the last day of each month.
@@ -24,6 +25,7 @@ while True:
         AI_report_maker(files, bucket_name)
 
     time.sleep(86400)
+
 
 
 
